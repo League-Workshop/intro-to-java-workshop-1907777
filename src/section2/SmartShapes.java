5010ff9c8faa739f661ba457436cd101726acf79
root <root@9d595cbd9982>
2020-04-28 23:58:59 +0000

    Candy Crush and Madlibs done working on world domination

--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -15,7 +15,16 @@ public class SmartShapes {
     	Sarah.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
+    	for (int i = 0; i < 4; i++) {
+    		Sarah.move(200);
+    		Sarah.turn(90);
+		}
     	
+    	for (int i = 0; i < 3; i++) {
+			Sarah.move(200);
+			Sarah.turn(60);
+			Sarah.move(200);
+		}
 
         //         2. Move your robot 200 pixels
 
